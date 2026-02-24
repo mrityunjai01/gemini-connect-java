8e3a7e11cef6e846d8d6c1234ad7b9f65a71176b
Mrityunjai <mrityunjaisingh3333@gmail.com>
2026-02-24 02:11:31 -0800

    write component scan and change applicationcontext call

--- src/main/java/com/queueco/app/App.java
+++ src/main/java/com/queueco/app/App.java
@@ -3,19 +3,16 @@ package com.queueco.app;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.queueco.app.v1.Handler;
-import com.queueco.app.v1.WebsocketClientConfig;
-
 @ComponentScan
 public class App {
     public static void main(String[] args) {
         try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
-                WebsocketClientConfig.class, Handler.class)) {
-
+                "com.queueco.app")) {
             Object webSocketManager = ctx.getBean("WebSocketConnectionManagerV1");
             Thread.currentThread().join();
 
         } catch (Exception exception) {
+            exception.printStackTrace();
 
         }
 
