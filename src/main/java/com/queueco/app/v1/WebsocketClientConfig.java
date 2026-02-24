f3102a39ec5e7394ee2a77dfd6623d6db71c16dd
Mrityunjai <mrityunjaisingh3333@gmail.com>
2026-02-24 02:11:11 -0800

    client config changed

--- src/main/java/com/queueco/app/v1/WebsocketClientConfig.java
+++ src/main/java/com/queueco/app/v1/WebsocketClientConfig.java
@@ -4,17 +4,21 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.WebSocketConnectionManager;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
-@Configuration
+@Lazy
+@Configuration("WebSocketClientConfigV1")
 public class WebsocketClientConfig {
+    @Lazy
     @Bean
     WebSocketClient websocketClient() {
         return new StandardWebSocketClient();
     }
 
+    @Lazy
     @Bean("WebSocketConnectionManagerV1")
     WebSocketConnectionManager websocketManager(@Qualifier("HandlerV1") Handler handler,
             WebSocketClient websocketClient) {
