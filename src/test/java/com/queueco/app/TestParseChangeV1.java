bb970bddcc896145e4b142b5d2abebdf181d3dbf
Mrityunjai <mrityunjaisingh3333@gmail.com>
2026-02-24 02:11:50 -0800

    change to iterable

--- src/test/java/com/queueco/app/TestParseChangeV1.java
+++ src/test/java/com/queueco/app/TestParseChangeV1.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
+import java.util.Iterator;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -23,60 +24,62 @@ public class TestParseChangeV1 {
     @Test
     public void parseChangeEmpty() {
         String changeMessage = "{}";
-        Iterable<PriceQty> iterPQ = this.changeMessageParser.parseMessage(changeMessage);
-        assertTrue(!iterPQ.iterator().hasNext());
+        Iterator<PriceQty> iterPQ = this.changeMessageParser.parseMessage(changeMessage);
+        assertTrue(!iterPQ.hasNext());
     }
 
     @Test
     public void parseSingeChange() {
         String changeMessage = "{\"eventId\":1764526788507910,\"events\":[{\"delta\":\"-2.227868\",\"price\":\"68180.45\",\"reason\":\"cancel\",\"remaining\":\"0.0\",\"side\":\"bid\",\"type\":\"change\"}],\"socket_sequence\":13,\"timestamp\":1771708700,\"timestampms\":1771708700841,\"type\":\"update\"}";
-        Iterable<PriceQty> iterPQ = this.changeMessageParser.parseMessage(changeMessage);
-        assertTrue(iterPQ.iterator().hasNext());
-        PriceQty pq = iterPQ.iterator().next();
+        Iterator<PriceQty> iterPQ = this.changeMessageParser.parseMessage(changeMessage);
+        // iterPQ.hasNext();
+        assertTrue(iterPQ.hasNext());
+        PriceQty pq = iterPQ.next();
         assertEquals(pq.getPrice(), new BigDecimal("68180.45"));
         assertEquals(pq.getQty(), new BigDecimal("0.0"));
-
-        assertTrue(!iterPQ.iterator().hasNext());
+        assertTrue(!pq.isAsk());
+        assertTrue(!iterPQ.hasNext());
     }
 
     @Test
     public void parseSingeChangeWithSpaces() {
-        String changeMessage = "{\"eventId\":1764526788507910,       \"events\":      [     {\"delta\":\"-2.227868\",\"price\":\"68180.45\",                 \"reason\":\"cancel\",   \"remaining\":\"0.0\",\"side\":\"bid\",\"type\":\"change\"}],\"socket_sequence\":13,\"timestamp\":1771708700,\"timestampms\":1771708700841,\"type\":\"update\"}";
-        Iterable<PriceQty> iterPQ = this.changeMessageParser.parseMessage(changeMessage);
-        assertTrue(iterPQ.iterator().hasNext());
-        PriceQty pq = iterPQ.iterator().next();
+        String changeMessage = "{\"eventId\":1764526788507910,       \"events\":      [     {\"delta\":\"-2.227868\",\"price\":\"68180.45\",                 \"reason\":\"cancel\",   \"remaining\":\"0.0\",\"side\":\"ask\",\"type\":\"change\"}],\"socket_sequence\":13,\"timestamp\":1771708700,\"timestampms\":1771708700841,\"type\":\"update\"}";
+        Iterator<PriceQty> iterPQ = this.changeMessageParser.parseMessage(changeMessage);
+        assertTrue(iterPQ.hasNext());
+        PriceQty pq = iterPQ.next();
         assertEquals(pq.getPrice(), new BigDecimal("68180.45"));
         assertEquals(pq.getQty(), new BigDecimal("0.0"));
-
-        assertTrue(!iterPQ.iterator().hasNext());
+        assertEquals(pq.getQty(), new BigDecimal("0.0"));
+        assertTrue(pq.isAsk());
     }
 
     @Test
     public void parseMissingChanges() {
         String changeMessage = "{\"eventId\":1764526788507910,\"socket_sequence\":13,\"timestamp\":1771708700,\"timestampms\":1771708700841,\"type\":\"update\"}";
-        Iterable<PriceQty> iterPQ = this.changeMessageParser.parseMessage(changeMessage);
-        assertTrue(!iterPQ.iterator().hasNext());
+        Iterator<PriceQty> iterPQ = this.changeMessageParser.parseMessage(changeMessage);
+        assertTrue(!iterPQ.hasNext());
     }
 
     @Test
     public void parseIncompleteChanges() {
         String changeMessage = "{\"eventId\":1764526788507910,\"events\":[{\"delta\":\"-2.227868\",\"price\":\"68180.45\",\"reason\":\"cancel\",\"side\":\"bid\",\"type\":\"change\"}],\"socket_sequence\":13,\"timestamp\":1771708700,\"timestampms\":1771708700841,\"type\":\"update\"}";
-        Iterable<PriceQty> iterPQ = this.changeMessageParser.parseMessage(changeMessage);
-        assertTrue(!iterPQ.iterator().hasNext());
+        Iterator<PriceQty> iterPQ = this.changeMessageParser.parseMessage(changeMessage);
+        assertTrue(!iterPQ.hasNext());
     }
 
     @Test
     public void parseMultipleChanges() {
         String changeMessage = "{\"eventId\":1764526788507910,\"events\":[{\"delta\":\"-2.227868\",\"price\":\"68180.45\",\"reason\":\"cancel\",\"remaining\":\"0.0\",\"side\":\"bid\",\"type\":\"change\"}, {\"delta\":\"-2.227868\",\"price\":\"63180.45\",\"reason\":\"cancel\",\"remaining\":\"1.0\",\"side\":\"bid\",\"type\":\"change\"}],\"socket_sequence\":13,\"timestamp\":1771708700,\"timestampms\":1771708700841,\"type\":\"update\"}";
-        Iterable<PriceQty> iterPQ = this.changeMessageParser.parseMessage(changeMessage);
-        assertTrue(iterPQ.iterator().hasNext());
-        PriceQty pq = iterPQ.iterator().next();
+        Iterator<PriceQty> iterPQ = this.changeMessageParser.parseMessage(changeMessage);
+        assertTrue(iterPQ.hasNext());
+        PriceQty pq = iterPQ.next();
         assertEquals(pq.getPrice(), new BigDecimal("68180.45"));
         assertEquals(pq.getQty(), new BigDecimal("0.0"));
-        assertTrue(iterPQ.iterator().hasNext());
-        pq = iterPQ.iterator().next();
+        assertTrue(!pq.isAsk());
+        assertTrue(iterPQ.hasNext());
+        pq = iterPQ.next();
         assertEquals(pq.getPrice(), new BigDecimal("63180.45"));
         assertEquals(pq.getQty(), new BigDecimal("1.0"));
-        assertTrue(!iterPQ.iterator().hasNext());
+        assertTrue(!iterPQ.hasNext());
     }
 }
